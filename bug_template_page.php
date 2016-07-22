fcb6edc56a5bb4fc39d7a540ba5720f7a9c14eaf
PC <tsvetoslavka@abv.bg>
2016-07-22 19:19:25 +0300

    testing

--- bug_template_page.php
+++ bug_template_page.php
@@ -46,8 +46,9 @@ mysql_select_db($db_database)or die("Unable to connect to database: " . mysql_er
 echo $name;*/
 
 define ("name", $_GET['name']);
+echo name;
 
-$query = "SELECT * FROM users, bugs WHERE bug_name = 'name' AND users.userID = bugs.userID";
+$query = "SELECT * FROM users, bugs WHERE bug_name = ".name." AND users.userID = bugs.userID";
 // execute the SQL query
 $result = mysql_query($query);
 if(!$result) die ("Could not query: " . mysql_error());
