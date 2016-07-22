c80c0d543245c19645e90a22be79f2504f96a8b7
PC <tsvetoslavka@abv.bg>
2016-07-22 17:20:01 +0300

    testing

--- bugs_page.php
+++ bugs_page.php
@@ -38,10 +38,12 @@ mysql_select_db($db_database)or die("Unable to connect to database: " . mysql_er
 <div class="container">
     <div class="starter-template">
 
-        $var = "Accidental semicolon";
+        <script type="text/javascript">
+        var = "Accidental semicolon";
+        </script>
 
         <!-- Send the variables myNumber=1 and myFruit="orange" to the new PHP page...-->
-        <a href="http://bughelp.azurewebsites.net/test.php?var=Accidental_semicolon">Send variables via URL!</a>
+        <a href="http://bughelp.azurewebsites.net/test.php?var">Send variables via URL!</a>
 
         <!--<li><a href = "http://bughelp.azurewebsites.net/test.php"> Accidental semicolon </a></li> -->
         <li><a href = "http://bughelp.azurewebsites.net/curly_braces.php"> Curly braces </a></li>
