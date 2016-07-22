34ab81923b3889311cb461a7dd99a42b929a6833
PC <tsvetoslavka@abv.bg>
2016-07-22 18:56:05 +0300

    testing

--- bug_template_page.php
+++ bug_template_page.php
@@ -47,16 +47,17 @@ echo $name;
 
 
 
-$query = "SELECT * FROM users, bugs WHERE bug_name = '.$name.' AND users.userID = bugs.userID";
+$query = "SELECT name,email, country FROM users, bugs WHERE bug_name = '".$name."' AND users.userID = bugs.userID";
 // execute the SQL query
 $result = mysql_query($query);
 if(!$result) die ("Could not query: " . mysql_error());
-$rows = mysql_num_rows($result);
+echo $result;
+/*$rows = mysql_num_rows($result);
 for($i = 0; $i < $rows; ++$i) {
     echo 'name: ' . mysql_result($result, $i, 'name') . '</br>';
     echo 'email: ' . mysql_result($result, $i, 'email') . '</br>';
     echo 'country: ' . mysql_result($result, $i, 'country') . '</br>';
-}
+}*/
 
 
 /*$query = "SELECT * FROM bugs";
