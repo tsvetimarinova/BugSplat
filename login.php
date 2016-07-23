c2f2cbaa4a081562f83106a87f202ff6854383a5
PC <tsvetoslavka@abv.bg>
2016-07-23 13:42:49 +0300

    testing

---
+++ login.php
@@ -0,0 +1,30 @@
+<?php
+session_start();
+if (isset($_POST['bttLogin'])){
+    require 'connect.php';
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $result = mysqli_query($con, 'select * from users where username = "'.$username.'" and password="'.$password.'"');
+    if (mysqli_num_rows($result)==1){
+        $_SESSION['username'] = $username;
+        header('Location: home_page.html');
+    }
+    else echo "Account is invalid";
+}
+?>
+<form method="post">
+    <table cellpadding="2" cellspacing="2" border="0">
+        <tr>
+            <td>Username:</td>
+            <td><input type="text" name="username"></td>
+        </tr>
+        <tr>
+            <td>Password:</td>
+            <td><input type="password" name="password"></td>
+        </tr>
+        <tr>
+            <td>$submit</td>
+            <td><input type="submit" name="bttLogin" value="Login"></td>
+        </tr>
+    </table>
+</form>
