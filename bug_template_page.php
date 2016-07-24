24ba7ac6c2ab1e9c9d33caa7eeb549894e7ea550
PC <tsvetoslavka@abv.bg>
2016-07-24 15:03:32 +0300

    test

--- bug_template_page.php
+++ bug_template_page.php
@@ -50,7 +50,7 @@ mysql_select_db($db_database)or die("Unable to connect to database: " . mysql_er
 $query = mysqli_query($con,'SELECT * FROM users, bugs WHERE bug_name = "'.$name.'" AND users.userID = bugs.userID');
 // execute the SQL query
 $result = mysqli_query($query);
-if(!$result) die ("Could not query: " . mysql_error());
+if(!$result) die ("Could not query: " . mysqli_error());
 $rows = mysql_num_rows($result);
 echo $rows;
 /*for($i = 0; $i < $rows; ++$i) {
