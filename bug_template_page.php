cded63e0f61a7e0497755fa0aeb8b8dd8bfe5e69
PC <tsvetoslavka@abv.bg>
2016-07-24 17:22:30 +0300

    test

--- bug_template_page.php
+++ bug_template_page.php
@@ -33,12 +33,6 @@ mysql_select_db($db_database)or die("Unable to connect to database: " . mysql_er
 <form>
     <fieldset>
         <legend>Please leave a comment...</legend>
-        <label for="name">Name:</label>
-        <input type="text" name="name" value="" />
-        <br>
-        <label for="email">Email:</label>
-        <input type="text" name="email" value="" />
-        <br>
         <label for="comments">Comment:</label>
         <textarea name="comments" cols="45" rows="5"></textarea>
         <br>
