ae1efeab4d0b757f38f3d7c8b7e58a8237bc31b4
PC <tsvetoslavka@abv.bg>
2016-07-24 17:15:08 +0300

    test

---
+++ test2.php
@@ -0,0 +1,19 @@
+<html>
+
+<form>
+    <fieldset>
+        <legend>Please leave a comment...</legend>
+        <label for="name">Name:</label>
+        <input type="text" name="name" value="" />
+        <br>
+        <label for="email">Email:</label>
+        <input type="text" name="email" value="" />
+        <br>
+        <label for="comments">Comment:</label>
+        <textarea name="comments" cols="45" rows="5"></textarea>
+        <br>
+        <input type="submit" value="Submit" />
+    </fieldset>
+</form>
+
+</html>
