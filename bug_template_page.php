af6f7248087e041f62d5fd308c7fc0e89cbda2a0
PC <tsvetoslavka@abv.bg>
2016-07-24 17:16:23 +0300

    test

--- bug_template_page.php
+++ bug_template_page.php
@@ -101,14 +101,16 @@ $db->close();
         <legend>Please leave a comment...</legend>
         <label for="name">Name:</label>
         <input type="text" name="name" value="" />
+        <br>
         <label for="email">Email:</label>
         <input type="text" name="email" value="" />
+        <br>
         <label for="comments">Comment:</label>
         <textarea name="comments" cols="45" rows="5"></textarea>
+        <br>
         <input type="submit" value="Submit" />
     </fieldset>
 </form>
-
 
 
 
