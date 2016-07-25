7da5fc546250241bb1d9054edf0c14b602a8da3a
PC <tsvetoslavka@abv.bg>
2016-07-25 17:42:52 +0300

    test

---
+++ search.php
@@ -0,0 +1,26 @@
+<?php
+
+
+
+
+?>
+
+<html>
+
+<form method="post">
+    <fieldset>
+        <legend>Search:</legend>
+        <table cellpadding="1" cellspacing="1" border="0">
+            <tr>
+                <td></td>
+                <td><input type="text" name="search"></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td><input type="submit" name="bttS" value="Search"></td>
+            </tr>
+        </table>
+    </fieldset>
+</form>
+
+</html>
