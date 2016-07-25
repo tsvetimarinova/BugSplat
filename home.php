3c6dd6f42dcd6c72fd76789bc706f0bd12318f28
PC <tsvetoslavka@abv.bg>
2016-07-25 18:50:58 +0300

    test

--- home.php
+++ home.php
@@ -60,7 +60,7 @@ echo 'Welcome, '.$_SESSION['username'];
 <?php
 require 'connect.php';
 $query1 = "SELECT * FROM bugs ORDER BY bugID DESC limit 5";
-$result3 = $db->query($query1);
+$result3 = $con->query($query1);
 
 if (!$result3) die ("Could not query: " . mysql_error());
 echo $result3;
