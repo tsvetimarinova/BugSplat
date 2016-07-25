4ec8bc944579f05040ae7d2b3bdecdd04d5164e7
PC <tsvetoslavka@abv.bg>
2016-07-25 19:30:38 +0300

    test

--- home.php
+++ home.php
@@ -71,6 +71,7 @@ require 'connect.php';
 $query1 = "SELECT * FROM bugs ORDER BY bugID DESC limit 5";
 $result3 = $con->query($query1);
 if (!$result3) die ("Could not query: " . mysqli_error());
+echo "PROBLEEEM!";
 echo $result3;
 
 ?>
