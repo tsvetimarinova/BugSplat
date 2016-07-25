1799bf912a4793834bf5039d963b604999547335
PC <tsvetoslavka@abv.bg>
2016-07-25 19:31:50 +0300

    test

--- home.php
+++ home.php
@@ -68,7 +68,7 @@ echo 'Welcome, '.$_SESSION['username'];*/
 */
 session_start();
 require 'connect.php';
-$query1 = "SELECT * FROM bugs ORDER BY bugID DESC limit 5";
+$query1 = "SELECT * FROM bugs ORDER BY bugID";
 $result3 = $con->query($query1);
 if (!$result3) die ("Could not query: " . mysqli_error());
 echo "PROBLEEEM!";
