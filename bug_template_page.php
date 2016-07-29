d452e6e14e8d3b55d78567fdf0aabcd1dc440bed
PC <tsvetoslavka@abv.bg>
2016-07-29 16:45:51 +0300

    test

--- bug_template_page.php
+++ bug_template_page.php
@@ -1,26 +1,3 @@
-<?php
-
-// connect to your Azure server and select database (remember you connection details are all on the azure portal
-//$db = new mysqli(
-// "eu-cdbr-azure-west-c.cloudapp.net",
-//"befd2db9fe76ad",
-// "5a698004",
-// "WebApp_Coursework"
-//);
-// test if connection was established, and print any errors
-//if($db->connect_error){
-//    die("Connection failed: " . $conn->connect_error);
-//}
-
-/*$db_database = 'databasebug1300608';
-$db_hostname = 'us-cdbr-azure-west-c.cloudapp.net';
-$db_username = 'b4bbf8767a3b3c';
-$db_password = '7ae9ed4b';
-$db_server = mysql_connect($db_hostname, $db_username, $db_password);
-if(!$db_server) die("Unable to connect to MYSQL: ". mysql_error());
-mysql_select_db($db_database)or die("Unable to connect to database: " . mysql_error());
-*/
-?>
 <html>
 <body>
 
@@ -44,11 +21,6 @@ mysql_select_db($db_database)or die("Unable to connect to database: " . mysql_er
 
 <?php
 
-//if ($_GET['bugs_page']== $var){
-// echo "link 1";
-//  } else {
-//      echo "link 2";
-//  }
 
 $db = new mysqli (
     "us-cdbr-azure-west-c.cloudapp.net",
@@ -59,36 +31,23 @@ $db = new mysqli (
 
 
     // Retrieve the URL variables (using PHP).
-    //$name = $_GET['name'];
-    //$name = $_SESSION [$name];
-
     session_start();
-    //echo 'Welcome '.$_SESSION[$name];
-    //echo $name;
 if (isset($_GET['name'])) {
     $date = date("Y-m-d");
     $comment = $_POST['comment'];
     require 'connect.php';
     $name = $_GET['name'];
     echo $name;
-    //$com =  "insert into comments values ('C006', '".$comment."', '".$date."', 'U001', 'B001')";
-    //$result = $db->query($com);
-    //if (!$result) die ("Could not query: " . mysql_error());
-    //else echo "Done!";
 
-    //$name2 = $_SESSION['name'];
-   // echo $name2;
+
+
     $query = "select * from bugs, users where bug_name = '".$name."' and users.userID = bugs.userID";
     $res = $db->query($query);
 // execute the SQL query
-//$result = mysql_query($query);
+
     if (!$res) die ("Could not query: " . mysql_error());
-    //$rows = mysqli_num_rows($res);
-//echo $rows;
-    //echo "name: ";
+
     while ($bug1 = mysqli_fetch_assoc($res)) {
-        // echo $bug['bug_name'] . " ";
-        //echo "<br />";
         echo "<br />";
         echo $bug1 ['name'];
         echo "<br />";
@@ -111,38 +70,17 @@ if (isset($_GET['name'])) {
     echo "<br />";
     echo "Commens: ";
     while ($com1 = mysqli_fetch_assoc($com_res)) {
-        // echo $bug['bug_name'] . " ";
-        //echo "<br />";
         echo "<br />";
         echo $com1 ['com_description'];
         echo "<br />";
 
 
     }
-    for ($i = 0; $i < $rows1; ++$i) {
-        echo 'Comment: ' . mysql_result($com, $i, 'com_description') . '</br>';
-    }
+
 }
 
-/*$query = "SELECT * FROM bugs";
-// execute the SQL query
-$result = mysql_query($query);
-if(!$result) die ("Could not query: " . mysql_error());
-$rows = mysql_num_rows($result);
-for($j = 0; $j < $rows; ++$j)
-{
-    echo ' ' . mysql_result($result, $j, 'bug_name' ) . '</br>';
-    echo \n;
-    // echo 'Description: ' . mysql_result($result, $j, 'bug_description') . '</br>';
-}
-// iterate over $result object one $row at a time
-// use fetch_array() to return an associative array
-//while($row = $result->fetch_array()){
-//  // print out fields from row of data
-//echo $row ;
-//}
-//
-*/
+
+
 $result->close();
 // close connection to database
 $db->close();
