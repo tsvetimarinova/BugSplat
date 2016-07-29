98d51f0327829c5486cac5ba642e7fae3c4fb881
PC <tsvetoslavka@abv.bg>
2016-07-29 17:32:37 +0300

    test

--- bug_template_page.php
+++ bug_template_page.php
@@ -8,7 +8,14 @@
     <li class="active"><a href = "http://bughelp.azurewebsites.net/search.php"> Search Page </a></li>
 </div>
 
-
+<form metod="post">
+    <fieldset>
+        <legend>Please leave a comment...</legend>
+        <textarea name="comments" cols="45" rows="5"></textarea>
+        <br>
+        <input type="submit" name= "bttCom" value="comment" />
+    </fieldset>
+</form>
 
 
 <?php
