deb1c16a88884a2f1a14f3351933d3a975a7a5e7
PC <tsvetoslavka@abv.bg>
2016-07-29 17:28:07 +0300

    test

--- bug_template_page.php
+++ bug_template_page.php
@@ -41,54 +41,53 @@ if (isset($_POST['bttCom'])) {
     $result_com = $db->query($insert_com);
     if (!$result_com) die ("Could not query: " . mysql_error());
     echo 'Your comment is added to the database. Please refresh the page to see it.';
-}
 
 
-if (isset($_GET['name'])) {
-    require 'connect.php';
-    $name = $_GET['name'];
-    echo $name;
+    if (isset($_GET['name'])) {
+        require 'connect.php';
+        $name = $_GET['name'];
+        echo $name;
 
 
-    $query = "select * from bugs, users where bug_name = '" . $name . "' and users.userID = bugs.userID";
-    $res = $db->query($query);
+        $query = "select * from bugs, users where bug_name = '" . $name . "' and users.userID = bugs.userID";
+        $res = $db->query($query);
 // execute the SQL query
 
-    if (!$res) die ("Could not query: " . mysql_error());
+        if (!$res) die ("Could not query: " . mysql_error());
 
-    while ($bug1 = mysqli_fetch_assoc($res)) {
-        echo "<br />";
-        echo 'Name: ' . $bug1 ['name'];
-        echo "<br />";
-        echo 'Email: ' . $bug1 ['email'];
-        echo "<br />";
-        echo 'Country: ' . $bug1 ['country'];
-        echo "<br />";
-        echo "<br />";
-        echo 'Bug: ' . $bug1 ['bug_description'];
+        while ($bug1 = mysqli_fetch_assoc($res)) {
+            echo "<br />";
+            echo 'Name: ' . $bug1 ['name'];
+            echo "<br />";
+            echo 'Email: ' . $bug1 ['email'];
+            echo "<br />";
+            echo 'Country: ' . $bug1 ['country'];
+            echo "<br />";
+            echo "<br />";
+            echo 'Bug: ' . $bug1 ['bug_description'];
 
-    }
+        }
 
 
-    $com = "SELECT * FROM comments, bugs WHERE bug_name = '" . $name . " ' AND comments.bugID = bugs.bugID";
-    $com_res = $db->query($com);
-    if (!$com_res) die ("Could not query: " . mysql_error());
-    //$rows1 = mysql_num_rows($com);
-    echo "<br />";
-    echo "<br />";
-    echo "<br />";
-    echo "Comments: ";
-    while ($com1 = mysqli_fetch_assoc($com_res)) {
+        $com = "SELECT * FROM comments, bugs WHERE bug_name = '" . $name . " ' AND comments.bugID = bugs.bugID";
+        $com_res = $db->query($com);
+        if (!$com_res) die ("Could not query: " . mysql_error());
+        //$rows1 = mysql_num_rows($com);
         echo "<br />";
-        echo $com1 ['com_description'];
         echo "<br />";
+        echo "<br />";
+        echo "Comments: ";
+        while ($com1 = mysqli_fetch_assoc($com_res)) {
+            echo "<br />";
+            echo $com1 ['com_description'];
+            echo "<br />";
+        }
+
+
     }
 
 
 }
-
-
-
 
 $result->close();
 // close connection to database
