10a79b27bf8ad2f96cd5d9cea1084ed0abf6834e
PC <tsvetoslavka@abv.bg>
2016-07-31 15:49:14 +0300

    test

--- bug_template_page.php
+++ bug_template_page.php
@@ -80,7 +80,7 @@ if (isset($_POST['bttCom'])) {
     $date = date("Y-m-d");
     $comment = $_POST['comments'];
 
-    $insert_com = "insert into comments values (NULL, '" . $comment . "', '" . $date . "', '2', '3')";
+    $insert_com = "insert into comments values ('', '" . $comment . "', '" . $date . "', '2', '3')";
     $result_com = $db->query($insert_com);
     if (!$result_com) die ("Could not query: " . mysql_error());
     echo 'Your comment is added to the database. Please refresh the page to see it.';
