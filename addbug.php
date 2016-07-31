19f853c70dbf2a2a6b7f7074298f8db3893f93e1
PC <tsvetoslavka@abv.bg>
2016-07-31 18:13:00 +0300

    test

---
+++ addbug.php
@@ -0,0 +1,21 @@
+<form method="post">
+    <fieldset>
+        <legend>Add Bug</legend>
+        <table cellpadding="2" cellspacing="2" border="0">
+            <tr>
+                <td>Bug name:</td>
+                <td><input type="text" name="bugname"></td>
+            </tr>
+            <td>Bug description</td>
+            <textarea name="decription" cols="45" rows="5"></textarea>
+            <tr>
+                <td>Tag:</td>
+                <td><input type="text" name="tag"></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td><input type="submit" name="bttAdd" value="Add"></td>
+            </tr>
+        </table>
+    </fieldset>
+</form>
