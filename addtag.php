c112a81e185224a3e7d1f694774a835c4fdaaabd
PC <tsvetoslavka@abv.bg>
2016-08-01 17:10:14 +0300

    test

---
+++ addtag.php
@@ -0,0 +1,44 @@
+<form method="post">
+    <fieldset>
+        <legend>Step 1: Please create a tag first: </legend>
+        <table cellpadding="2" cellspacing="2" border="0">
+            <tr>
+                <td>Tag:</td>
+                <td><input type="text" name="tag"></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td><input type="submit" name="bttTag" value="Create tag"></td>
+            </tr>
+        </table>
+    </fieldset>
+</form>
+
+
+
+<?php
+
+session_start();
+
+$db = new mysqli (
+    "us-cdbr-azure-west-c.cloudapp.net",
+    "b4bbf8767a3b3c",
+    "7ae9ed4b",
+    "databasebug1300608"
+);
+
+
+if (isset($_POST['bttTag'])) {
+
+    $tag = $_POST['tag'];
+
+
+
+    $inserttag = "INSERT INTO tags VALUES (NULL, '" . $tag . "')";
+    $resulttag = $db->query($inserttag);
+    if (!$resulttag) die ("Could not query: " . mysqli_error($db));
+    echo 'Your tag is added to the database.';
+}
+
+
+?>
