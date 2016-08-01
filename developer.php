6e24315f89fc63e22e358859458c3c34d0c6014c
PC <tsvetoslavka@abv.bg>
2016-08-01 19:27:16 +0300

    test

--- developer.php
+++ developer.php
@@ -12,6 +12,7 @@
 <?php
 session_start();
 $userid = $_SESSION ['id'];
+echo $userid;
 $user = "SELECT * FROM users, WHERE userID = '".$userid."'";
 $res = $db->query($user);
 if (!$res) die ("Could not query: " . mysqli_error($db));
@@ -28,9 +29,9 @@ while ($user1 = mysqli_fetch_assoc($res)) {
 $query2 = "SELECT bug_name FROM bugs, users WHERE bugs.userID = users.userID AND users.userID = '".$userid."'";
 // execute the SQL query
 $result2 = $db->query($query2);
-if(!$result2) die ("Could not query: " . mysql_error());
+if(!$result2) die ("Could not query: " . mysqli_error($db));
 $rows = mysql_num_rows($result2);
-if (!$result2) die ("Could not query: " . mysql_error());
+if (!$result2) die ("Could not query: " . mysqli_error($db));
 while ($bug = mysqli_fetch_assoc($result2)){
     echo '<a href="http://bughelp.azurewebsites.net/bug_template_page.php?name='.$bug['bug_name'].'">'.$bug['bug_name'].'</a>';
     echo "<br />";
