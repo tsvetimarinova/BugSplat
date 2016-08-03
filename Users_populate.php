0f559d56663e70a38e47fbff0089f11a8aae8994
PC <tsvetoslavka@abv.bg>
2016-08-03 13:56:15 +0300

    test

--- Users_populate.php
+++ Users_populate.php
@@ -13,12 +13,12 @@ if ($conn->connect_error){
 }
 /*Populates sql table*/
 $sql = "
-INSERT INTO Users (userID, name, email, country, developer, admin) VALUES ('U001', 'Tsvetoslava Marinova', 'tsvetoslavka@abv.bg', 'Bulgaria', '1', '1', 'PR01');
-INSERT INTO Users (userID, name, email, country, developer, admin) VALUES ('U002', 'Hristina Dimitrova', 'h.v.dimitrova@gmail.com', 'Bulgaria', '1', '0', 'PR01');
-INSERT INTO Users (userID, name, email, country, developer, admin) VALUES ('U003', 'Georgi Parapanov', 'g.parapanov@gmail.com', 'Germany', '0', '1', 'PR01');
-INSERT INTO Users (userID, name, email, country, developer, admin) VALUES ('U004', 'Konstantin Velchevski', 'k.vel@gmail.com', 'USA', '1', '0', 'PR01');
-INSERT INTO Users (userID, name, email, country, developer, admin) VALUES ('U005', 'Martin Petrov', 'm.petrov@gmail.com', 'Egypt', '1', '0', 'PR01');
-INSERT INTO Users (userID, name, email, country, developer, admin) VALUES ('U006', 'Vasil Mitsev', 'v.p.mitsev@gmail.com', 'Australia', '1', '0', 'PR01');
+INSERT INTO Users (userID, name, email, country, developer, admin) VALUES ('1', 'Tsvetoslava Marinova', 'tsvetoslavka@abv.bg', 'Bulgaria', '1', '1', 'PR01', 'tsveti', '123456');
+INSERT INTO Users (userID, name, email, country, developer, admin) VALUES ('2', 'Hristina Dimitrova', 'h.v.dimitrova@gmail.com', 'Bulgaria', '1', '0', 'PR01', 'h.dimitrova', '23456');
+INSERT INTO Users (userID, name, email, country, developer, admin) VALUES ('3', 'Georgi Parapanov', 'g.parapanov@gmail.com', 'Germany', '0', '1', 'PR01', 'georgi', 'g12345');
+INSERT INTO Users (userID, name, email, country, developer, admin) VALUES ('4', 'Konstantin Velchevski', 'k.vel@gmail.com', 'USA', '1', '0', 'PR01', 'koko.vel', 'koko5');
+INSERT INTO Users (userID, name, email, country, developer, admin) VALUES ('5', 'Martin Petrov', 'm.petrov@gmail.com', 'Egypt', '1', '0', 'PR01', 'martin', 'petrov');
+INSERT INTO Users (userID, name, email, country, developer, admin) VALUES ('6', 'Vasil Mitsev', 'v.p.mitsev@gmail.com', 'Australia', '1', '0', 'PR01', 'vasil.m', '123456');
 ";
 
 
